9f6be79aabdf4cb6274016e2ca32bc2eaf920414
aleenaaahad <aleenaahad5@gmail.com>
2025-12-04 18:16:27 -0800

    changed db connection file

--- backend/src/com/movierental/database/DatabaseConnection.java
+++ backend/src/com/movierental/database/DatabaseConnection.java
@@ -1,15 +1,45 @@
 package com.movierental.database;
 
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class DatabaseConnection {
-private static final String URL = System.getenv("DB_URL");
-private static final String USERNAME = System.getenv("DB_USERNAME");
-private static final String PASSWORD = System.getenv("DB_PASSWORD");
+
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
+
+    static {
+        try {
+            Properties props = new Properties();
+
+            String path = DatabaseConnection.class
+                    .getClassLoader()
+                    .getResource("config.properties")
+                    .getPath();
+
+            try (InputStream input = DatabaseConnection.class.getResourceAsStream("/config.properties")) {
+                if (input == null) {
+                    throw new RuntimeException("config.properties not found");
+                }
+
+                props.load(input);
+            }
+
+            URL = props.getProperty("db.url");
+            USERNAME = props.getProperty("db.username");
+            PASSWORD = props.getProperty("db.password");
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Database error: " + e.getMessage());
+        }
+    }
 
     public static Connection getConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (Exception e) {
             throw new RuntimeException("Database error: " + e.getMessage());
